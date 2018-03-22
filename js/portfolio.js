66e7311fb6d4cddf07639d297bf01393458a32d5
wehbs <shamirwehbe@me.com>
2018-03-22 11:55:08 -0400

    made colors match code wallpaper and keyboard

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,145 +1,161 @@
-$(function () {
+$(function() {
+  // $.getJSON("https://ipapi.co/json/",
+  //   function (json) {
+  //     var config = {
+  //       apiKey: "AIzaSyDxW2ypEVCUqBXGf-gL7MXA0pUJjEiPxU0",
+  //       authDomain: "portfolio-5daeb.firebaseapp.com",
+  //       databaseURL: "https://portfolio-5daeb.firebaseio.com",
+  //       projectId: "portfolio-5daeb",
+  //       storageBucket: "portfolio-5daeb.appspot.com",
+  //       messagingSenderId: "667750983442"
+  //     };
+  //     firebase.initializeApp(config);
+  //     var database = firebase.database();
+  //     var ipValues = {
+  //       city: json.city,
+  //       country: json.country_name,
+  //       isp: json.org,
+  //       lat: json.latitude,
+  //       lon: json.longitude,
+  //       ip: json.ip,
+  //       state: json.region,
+  //       zip: json.postal,
+  //       timestamp: firebase.database.ServerValue.TIMESTAMP,
+  //     };
+  //     database.ref().push(ipValues);
+  //   });
 
+  // AniView for AnimationEvent.CSS
+  var options = {
+    animateThreshold: 1,
+    scrollPollInterval: 20
+  };
+  $(".aniview").AniView(options);
 
-
-// $.getJSON("https://ipapi.co/json/",
-//   function (json) {
-//     var config = {
-//       apiKey: "AIzaSyDxW2ypEVCUqBXGf-gL7MXA0pUJjEiPxU0",
-//       authDomain: "portfolio-5daeb.firebaseapp.com",
-//       databaseURL: "https://portfolio-5daeb.firebaseio.com",
-//       projectId: "portfolio-5daeb",
-//       storageBucket: "portfolio-5daeb.appspot.com",
-//       messagingSenderId: "667750983442"
-//     };
-//     firebase.initializeApp(config);
-//     var database = firebase.database();
-//     var ipValues = {
-//       city: json.city,
-//       country: json.country_name,
-//       isp: json.org,
-//       lat: json.latitude,
-//       lon: json.longitude,
-//       ip: json.ip,
-//       state: json.region,
-//       zip: json.postal,
-//       timestamp: firebase.database.ServerValue.TIMESTAMP,
-//     };
-//     database.ref().push(ipValues);
-//   });
-
-
-// AniView for AnimationEvent.CSS
-$('.aniview').AniView();
-
-// jQuery to collapse the navbar shadow on scroll
-// =================================================
-function removeNavShadow () {
+  // jQuery to collapse the navbar shadow on scroll
+  // =================================================
+  function removeNavShadow() {
     var scroll = $(window).scrollTop();
     if (scroll === 0) {
       $("nav.white").addClass("z-depth-0");
-    }
-      else {
+    } else {
       $("nav.white").removeClass("z-depth-0");
-      }
     }
-removeNavShadow();
-$(window).scroll(function () {
-      removeNavShadow();
-  });
-// =================================================
-
-
-// jQuery to remove valign-wrapper on small screen because is breaks the columns
-// =================================================
-function checkWidth () {
-  if ($(window).width() < 600) {
-    $("#about").removeClass("valign-wrapper");
-} else {
-  $("#about").addClass("valign-wrapper");
   }
-}
-checkWidth();
-$(window).resize(checkWidth);
-// =================================================
+  removeNavShadow();
+  $(window).scroll(function() {
+    removeNavShadow();
+  });
+  // =================================================
+
+  // jQuery to remove valign-wrapper on small screen because is breaks the columns
+  // =================================================
+  function checkWidth() {
+    if ($(window).width() < 600) {
+      $("#about").removeClass("valign-wrapper");
+      $("#skills").removeClass("valign-wrapper");
+    } else {
+      $("#about").addClass("valign-wrapper");
+      $("#skills").addClass("valign-wrapper");
+    }
+  }
+  checkWidth();
+  $(window).resize(checkWidth);
+  // =================================================
 
 
 
+  $('.b1').whenInViewport(function() {
+    $('.b1').addClass('bar1');
+  });
+  $('.b2').whenInViewport(function() {
+    $('.b2').addClass('bar2');
+  });
+  $('.b3').whenInViewport(function() {
+    $('.b3').addClass('bar3');
+  });
+  $('.b4').whenInViewport(function() {
+    $('.b4').addClass('bar4');
+  });
+  $('.b5').whenInViewport(function() {
+    $('.b5').addClass('bar5');
+  });
+  $('.b6').whenInViewport(function() {
+    $('.b6').addClass('bar6');
+  });$('.b7').whenInViewport(function() {
+    $('.b7').addClass('bar7');
+  });$('.b8').whenInViewport(function() {
+    $('.b8').addClass('bar8');
+  });
 
+  // Click anywhere on the terminal window and it will focus to the text box so that you can type commands
+  // $(".shell-wrap").click(function () {
+  //   $(".shell-box").focus();
+  // });
 
+  // // Navigate the site through terminal commands
+  // function terminal(element) {
+  //   if (event.key === "Enter") {
 
+  //     var command = element.value.toLowerCase();
+  //     $("<li>" + command + "</li>").insertBefore(".shell-box");
+  //     $(".shell-box").val("");
 
+  //     switch (command) {
 
-// Click anywhere on the terminal window and it will focus to the text box so that you can type commands
-// $(".shell-wrap").click(function () {
-//   $(".shell-box").focus();
-// });
+  //       case "help":
+  //         $("<li>cd home: scroll to top of the page</li> <li>cd about: scroll to about section</li> <li>cd portfolio: scroll to portfolio section</li> <li>cd contact: scroll to contact section</li> <li>cd github: go to my github</li> <li>cd linkedin: go to my linkedin</li> <li>send email: send me an email</li> <li>reload: reload the webpage</li> <li>clear: empty the bash window</li> <li>hide: hide the bash window</li>").insertBefore(".shell-box");
+  //         break;
 
-// // Navigate the site through terminal commands
-// function terminal(element) {
-//   if (event.key === "Enter") {
+  //       case "cd home":
+  //         $('html, body').stop().animate({
+  //           scrollTop: $("#page-top").offset().top
+  //         }, 1500, 'easeInOutExpo');
+  //         break;
 
-//     var command = element.value.toLowerCase();
-//     $("<li>" + command + "</li>").insertBefore(".shell-box");
-//     $(".shell-box").val("");
+  //       case "cd about":
+  //         $('html, body').stop().animate({
+  //           scrollTop: $("#about").offset().top
+  //         }, 1500, 'easeInOutExpo');
+  //         break;
 
-//     switch (command) {
+  //       case "cd portfolio":
+  //         $('html, body').stop().animate({
+  //           scrollTop: $("#portfolio").offset().top
+  //         }, 1500, 'easeInOutExpo');
+  //         break;
 
-//       case "help":
-//         $("<li>cd home: scroll to top of the page</li> <li>cd about: scroll to about section</li> <li>cd portfolio: scroll to portfolio section</li> <li>cd contact: scroll to contact section</li> <li>cd github: go to my github</li> <li>cd linkedin: go to my linkedin</li> <li>send email: send me an email</li> <li>reload: reload the webpage</li> <li>clear: empty the bash window</li> <li>hide: hide the bash window</li>").insertBefore(".shell-box");
-//         break;
+  //       case "cd contact":
+  //         $('html, body').stop().animate({
+  //           scrollTop: $("#contact").offset().top
+  //         }, 1500, 'easeInOutExpo');
+  //         break;
 
-//       case "cd home":
-//         $('html, body').stop().animate({
-//           scrollTop: $("#page-top").offset().top
-//         }, 1500, 'easeInOutExpo');
-//         break;
+  //       case "cd github":
+  //         window.location = "https://github.com/wehbs";
+  //         break;
 
-//       case "cd about":
-//         $('html, body').stop().animate({
-//           scrollTop: $("#about").offset().top
-//         }, 1500, 'easeInOutExpo');
-//         break;
+  //       case "cd linkedin":
+  //         window.location = "https://www.linkedin.com/in/shamirwehbe";
+  //         break;
 
-//       case "cd portfolio":
-//         $('html, body').stop().animate({
-//           scrollTop: $("#portfolio").offset().top
-//         }, 1500, 'easeInOutExpo');
-//         break;
+  //       case "send email":
+  //         window.location = "mailto:shamirwehbe@me.com";
+  //         break;
 
-//       case "cd contact":
-//         $('html, body').stop().animate({
-//           scrollTop: $("#contact").offset().top
-//         }, 1500, 'easeInOutExpo');
-//         break;
+  //       case "reload":
+  //         location.reload();
+  //         break;
 
-//       case "cd github":
-//         window.location = "https://github.com/wehbs";
-//         break;
+  //       case "clear":
+  //         $(".shell-body li.pwd").nextAll("li").remove();
+  //         break;
 
-//       case "cd linkedin":
-//         window.location = "https://www.linkedin.com/in/shamirwehbe";
-//         break;
+  //       case "hide":
+  //         $(".shell-wrap").remove();
+  //         break;
 
-//       case "send email":
-//         window.location = "mailto:shamirwehbe@me.com";
-//         break;
-
-//       case "reload":
-//         location.reload();
-//         break;
-
-//       case "clear":
-//         $(".shell-body li.pwd").nextAll("li").remove();
-//         break;
-
-//       case "hide":
-//         $(".shell-wrap").remove();
-//         break;
-
-//     }
-//   }
-// };
-
-
+  //     }
+  //   }
+  // };
 });
